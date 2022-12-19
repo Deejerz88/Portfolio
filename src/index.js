0d7956d6839fe9ccd37509be5fec395f6f977847
Deejerz88 <djmcmillan93@gmail.com>
2022-12-19 11:42:39 -0500

    serviceworkerregistration.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorkerRegistration";
+import * as serviceWorker from "./serviceWorkerRegistration.js";
 
 serviceWorker.register({
   onUpdate: (registration) => {
